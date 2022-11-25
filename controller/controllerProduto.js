50ffc7dbd6a8e332ae9ab227ef9caab462513fc3
Eduardo Perucci <peruccii2917@hotmail.com>
2022-11-24 22:38:59 -0300

    first commit

---
+++ controller/controllerProduto.js
@@ -0,0 +1,61 @@
+ /*
+
+    Objetivo: Arquivo responsável pela manipulação de dados com o BD (insert, update, delete e select)
+    Autor: Eduardo Perucci
+    Data de criação: 31/10/22
+    Última modificação em: 31/10/22
+    Versão: 1.0 
+
+*/
+
+const { PrismaClientRustPanicError } = require('@prisma/client/runtime/index.js')
+const {MESSAGE_ERROR, MESSAGE_SUCCESS} = require('../modulo/config.js')
+const produto = require('../model/DAO/produto.js')
+
+
+const listarProdutos = async function(){
+    let dadosProdutosJSON = {}
+
+    const { selectAllProdutos } = require('../model/DAO/produto.js')
+
+    const dadosProdutos = await selectAllProdutos()
+
+    if (dadosProdutos) {
+
+        /* dadosAlunos.forEach(element => {
+            element.id = Number(element.id)
+        }); */
+
+        dadosProdutosJSON.produtos = dadosProdutos
+        return dadosProdutosJSON
+        
+    } else{
+        return false
+    }
+
+}
+
+const listarPizzas = async function(){
+let pizzasJSON = {}
+
+//produto = caminho para eu selecionar os selects 
+const dados = await produto.selectAllPizzas()
+
+if (dados) {
+    pizzasJSON.message = dados
+    pizzasJSON.status = 200
+} else{
+    pizzasJSON.message = MESSAGE_ERROR.NOT_FOUND_DB
+    pizzasJSON.status = 404
+}
+
+return pizzasJSON
+
+}
+
+
+
+module.exports = {
+    listarProdutos
+  
+}
---
+++ model/DAO/login.js
@@ -0,0 +1,39 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const insertUser = async (user) => {
+
+    try{
+
+    const sql = `insert into tbl_login(usuario,senha) values(('${user.usuario}'),'${user.senha}')`
+
+    const result = await prisma.$executeRawUnsafe (sql);
+
+    if (result) {
+        return true;
+    }else
+        return false;
+
+
+}catch (error) {
+    return false;
+    }
+}
+
+const selectUser = async () => {
+
+    const sql = `select * from tbl_login order by id desc`
+
+    const dados = await prisma.$queryRawUnsafe(sql)
+
+    if (dados.length > 0) {
+        return dados
+    } else{
+        return false
+    }
+}
+
+module.exports = {
+    selectUser,
+    insertUser
+}
